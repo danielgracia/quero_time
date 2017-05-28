9903ddc2cdf54993c2b2d3050bde4ada42215bf3
Daniel Gracia <daniel.santos@redealumni.com>
2017-05-28 02:05:08 -0300

    Set correct URL hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,11 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  # Set correct URLs
+  config.action_controller.default_url_options = {
+    host: 162.243.69.190
+  }
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
