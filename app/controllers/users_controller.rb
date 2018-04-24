bac408fa40b58821f8ff2383a9675d4d2d540c51
Daniel Silverio <danielmsilverio@gmail.com>
2018-04-23 23:21:30 -0300

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
   before_action :set_user, only: [:show, :edit, :update, :team_give_up]
   before_action :check_user, only: [:edit, :update]
 
